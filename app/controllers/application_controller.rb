d9aa93f7a34f031591c297d04700e860997b6af0
Mazen A. Melibari <mazen@mazen.ws>
2013-01-10 09:02:42 -0500

    Added the leaderboard to the side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :layout_logic
 
+  protected
   def login_required
     if not current_user
       flash.now[:warning] = t(:youHaveToLoginFirst)
@@ -9,8 +11,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
+  protected
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  protected
+  def layout_logic
+    @top_users = User.order("(comments_karma + posts_karma) DESC").limit(10)
   end
 end
