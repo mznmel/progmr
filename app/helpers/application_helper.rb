4f30c9641e2d269d77250f65b3113e03c2297c3f
Mazen A. Melibari <mazen@mazen.ws>
2013-02-07 01:25:18 -0500

    removing fonts.com fonts and adding gravatar in the leaderboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def markdown(text)
     Kramdown::Document.new(text).to_html.html_safe
   end
+
+  def gravatar(email)
+      emailMd5 = Digest::MD5.hexdigest(email)
+      "http://gravatar.com/avatar/#{emailMd5}?s=32"
+  end
 end
