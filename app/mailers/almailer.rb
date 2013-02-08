2fcf9d03dc6ea41afe77748944cd37f60052c2f3
Mazen A. Melibari <mazen@mazen.ws>
2013-02-08 01:48:08 -0500

    change from email address

--- app/mailers/almailer.rb
+++ app/mailers/almailer.rb
@@ -1,5 +1,5 @@
 class Almailer < ActionMailer::Base
-  default from: "info@progmr.com"
+  default from: "Progmr@Progmr.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
