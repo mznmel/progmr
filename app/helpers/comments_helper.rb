6eb261d8dedcac774b092f263bb1232f3837e327
Mazen A. Melibari <mazen@mazen.ws>
2013-02-07 22:10:26 -0500

    improve comments views

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,8 @@
 module CommentsHelper
   def nested_comments(comments)
     comments.map do |comment, childs|
-      render(comment) + content_tag(:div, nested_comments(childs), :class => "nested nestedComment-#{comment.id}" )
+      chds = content_tag(:div, nested_comments(childs), :class => "nested nestedComment-#{comment.id}")
+      render(comment, :childs => chds)
     end.join.html_safe
   end
 end
